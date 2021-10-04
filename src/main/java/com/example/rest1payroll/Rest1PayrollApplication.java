6029f5cdf3c0c0e489b15824a69900626b4f0c86
erfanrazi <erfan.flash.69@gmail.com>
2021-10-04 18:16:54 +0330

    Initial commit

---
+++ src/main/java/com/example/rest1payroll/Rest1PayrollApplication.java
@@ -0,0 +1,13 @@
+package com.example.rest1payroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rest1PayrollApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Rest1PayrollApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rest1payroll/Rest1PayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rest1payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rest1PayrollApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
